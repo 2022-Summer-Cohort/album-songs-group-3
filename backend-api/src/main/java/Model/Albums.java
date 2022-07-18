fc7643b3180c9b5764cdd4d37711ec79440df99a
TMBrunetti <tmb2192@gmail.com>
2022-07-18 15:40:23 -0400

    established MVC Repo Populator working on album

--- backend-api/src/main/java/Model/Albums.java
+++ backend-api/src/main/java/Model/Albums.java
@@ -1,4 +1,20 @@
 package Model;
 
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Collection;
+
+@Entity
 public class Albums {
+    @Id @GeneratedValue
+    private long id;
+
+    private String albumName;
+    private String recordLabel;
+    private Collection<Songs> songs;
+
+
+
 }
