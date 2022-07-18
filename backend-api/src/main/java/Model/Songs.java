2c0b291bd679c7972048323c92a384e833285e38
TMBrunetti <tmb2192@gmail.com>
2022-07-18 15:27:46 -0400

    creating POJOs and what not

---
+++ backend-api/src/main/java/Model/Songs.java
@@ -0,0 +1,18 @@
+package Model;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Songs {
+
+            @Id @GeneratedValue
+            private String songName;
+
+
+
+
+
+}
