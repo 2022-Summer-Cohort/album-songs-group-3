089501628009635e62c6afe22949c75d6a1f8329
Eddie <tastyCanOfMalk@users.noreply.github.com>
2022-07-20 14:11:06 -0400

    front end JS working sorta

--- frontend-spa/src/js/app.js
+++ frontend-spa/src/js/app.js
@@ -7,6 +7,7 @@ fetch("http://localhost:8080/api/albums")
 .then(albums => {
     console.log(albums);
     const container = document.querySelector(".container");
+
     container.innerHTML += header();
     container.innerHTML += home(albums);
     container.innerHTML += footer();
