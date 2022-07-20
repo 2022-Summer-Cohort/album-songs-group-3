dceff4cdedfe88864da7044d9aa9cd3bda47bcd8
Eddie <tastyCanOfMalk@users.noreply.github.com>
2022-07-20 14:14:06 -0400

    working homeJS

--- frontend-spa/src/js/home.js
+++ frontend-spa/src/js/home.js
@@ -1,29 +1,25 @@
 export default function home(albums){
     return `
-    <main class="main-content">
-        <section class="album-library">
-            ${albums.map(album =>{
-                return `
-                <div class="row row-cols-1 row-cols-md-3 g-4">
-                    <div class="col">
-                        <div class="card">
-                            <img class="card-img-top" src = ${album.imgUrl}>
-                            <div class="card-body>
-                                <h5 class="card-title">${album.name}</h5>
+        <div class="containter m-3">
+            <div class="row row-cols-1 row-cols-md-3 g-4">
+                ${albums.map(album =>{
+                    return `
+
+                        <div class="col">
+                            <div class="card h-100 p-1">
+                                <img class="card-img-top" src="${album.imgUrl}">
+                                <div class="card-body>
+                                    <h5 class="card-title">${album.artist} - ${album.name} (${album.label})</h5>
+                                    <p class="card-text">${album.description}</p>
+                                    <a href="#" class="btn btn-sm btn-primary">More Info</a>
+                                </div>
                             </div>
-                        <div>
-                    </div>
-                </div>
-                        <h1>${album.name}</h1>
-                        <h2>${album.artist}</h2>
-                        <h3>${album.label}</h3>
-                        <p>${album.description}</p>
-                        <img src= ${album.imgUrl}>
-                `
-            }).join("")
-        }
-    
-        </section>
-    </main>
+                        </div>
+                        
+                        `
+                    }).join("")
+                }
+            </div>
+    </div>
     `
 }
