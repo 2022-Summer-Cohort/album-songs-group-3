ae72c689f568c9e80d50eb22add16d5cfbba76c7
Eddie <tastyCanOfMalk@users.noreply.github.com>
2022-07-21 13:45:35 -0400

    album songs basic structure

--- frontend-spa/src/js/albumView.js
+++ frontend-spa/src/js/albumView.js
@@ -1,9 +1,34 @@
 export default function albumView(album){
-    return `
+  
+  return `
+  
+    <div class="containter m-3">
+        <div class="row">
+            
+            <div class="albumElement col-4">
+                <div class="card h-100">
 
-    <h2>${album.artist}</h2>
-    <h2>${album.artist}</h2>
-    <h2>${album.artist}</h2>
+                    <img class="card-img-top" src="${album.imgUrl}">
+                    <div class="card-body">
+                        <h5 class="card-title">${album.artist} - ${album.name} (${album.label})</h5>
+                        <p class="card-text">${album.description}</p>
+                        <a href="#" class="albumButton btn btn-sm btn-primary">More Info</a>
+                        <input class="id_field" hidden value="${album.id}">
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="col-8">
+                <ol class="list-group list-group-numbered">
+                    <li class="list-group-item">A list item</li>
+                    <li class="list-group-item">A list item</li>
+                    <li class="list-group-item">A list item</li>
+                </ol>
+            </div>
+
+        </div>
+    </div>
 
     `
 }
